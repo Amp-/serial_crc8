ad8d6c461cfdf0de94c80ba42888144ce487973d
Andrey Gushchin <gushchin89@gmail.com>
2023-07-10 22:58:47 +0300

    test thread

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import serial
 import uart
+import threading
 import crc_maxim
 import test
 method = 'com'
 port = '/dev/ttyUSB0'
 baudrate = '115200'
-parity = 'PARITY_EVEN'
 
+in_list = []
+out_flag = 0
+ser = serial.Serial(port,baudrate,timeout=1)
 
 def serail_data(com:uart.Serial):
     with serial.Serial() as ser:
@@ -31,6 +34,21 @@ def write_2():
         t = ser.read(4)
         print(t[1:2])
 
+def fn_in():
+    global in_list
+    while True:
+        in_len = 0
+        while in_len < 1:
+            in_st = ser.read(4)
+            in_len = len(in_st)
+        in_list.append(in_st)
+        print(in_st)
+
+def fn_out():
+    global out_flag
+    out_flag = 1
+def fn_send():
+    ser.write(b'\xaa\x01\x00\xc4')
 def read_data_2():
     ser = serial.Serial(port, baudrate, timeout=1)
     t = ser.read(4)
@@ -51,8 +69,16 @@ if __name__ == '__main__':
     # write_date(com)
     # t = read_data(data)
     # print(t)
-    while True:
-        write_2()
+    # while True:
+    #     write_2()
         # read_data_2()
+    while True:
+        t = input(': ')
+        fn_send()
+        tr_in = threading.Thread(target = fn_in())
+        tr_in.daemon = True
+        tr_in.start()
+
+
 
 
